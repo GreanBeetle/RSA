cbef23cddbf80623ab549f7ff6ae9060a4cf37bd
John Rykken <john.rykken@gmail.com>
2020-10-25 18:58:39 -0700

    finish hello world of Animations API

--- App.js
+++ App.js
@@ -1,37 +1,45 @@
 import React, { useState } from 'react'
-import { Dimensions, StyleSheet, View, Text, SafeAreaView} from 'react-native'
+import { 
+  Dimensions, 
+  StyleSheet, 
+  View, 
+  Text, 
+  SafeAreaView,
+  Animated, 
+  TouchableOpacity
+} from 'react-native'
 import { useAssets } from 'expo-asset'
 import { Video } from 'expo-av'
 import { AppLoading } from 'expo'
 import COLORS from './src/colors'
 
 const App = () => {
+  let content = <AppLoading />
   /************************************************ 
   VIDEO 
   *************************************************/
-  let content = <AppLoading />
-  const [assets] = useAssets([
-    require('./assets/fire.mp4'),
-    require('./assets/nightsky.mp4'),
-    require('./assets/waves.mp4')
-  ])
+  // const [assets] = useAssets([
+  //   require('./assets/fire.mp4'),
+  //   require('./assets/nightsky.mp4'),
+  //   require('./assets/waves.mp4')
+  // ])
 
-  const video = (
-    <SafeAreaView style={styles.container}>
-      <Video
-        source={require('./assets/nightsky.mp4')}
-        rate={1.0}
-        volume={1.0}
-        isMuted={false}
-        resizeMode={Video.RESIZE_MODE_COVER}
-        shouldPlay
-        isLooping
-        style={{width: 300, height: 300}} // MOVE STYLE OUT 
-      />
-    </SafeAreaView>
-  )
+  // const video = (
+  //   <SafeAreaView style={styles.container}>
+  //     <Video
+  //       source={require('./assets/nightsky.mp4')}
+  //       rate={1.0}
+  //       volume={1.0}
+  //       isMuted={false}
+  //       resizeMode={Video.RESIZE_MODE_COVER}
+  //       shouldPlay
+  //       isLooping
+  //       style={{width: 300, height: 300}} // MOVE STYLE OUT 
+  //     />
+  //   </SafeAreaView>
+  // )
 
-  if (assets) content = video 
+  // if (assets) content = video 
   /************************************************
   VIDEO 
   *************************************************/
@@ -40,8 +48,35 @@ const App = () => {
   /************************************************
   ANIMATIONS
   *************************************************/
-  const animated = <View style={{height: 300, width: 300, backgroundColor: COLORS.actionGreen }}/> // MOVE STYLE OUT
+  const value = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0]
+
+  function moveBall() {
+    Animated.timing(value, {
+      toValue: { x: 100, y: 100 }, 
+      duration: 1000, 
+      useNativeDriver: false
+    }).start()
+  }
+
+  const animated = (
+    <View style={styles.container}>
+      <Animated.View style={value.getLayout()}>
+        <View 
+          style={{
+            height: 100, 
+            width: 100,
+            borderRadius: 50, 
+            backgroundColor: COLORS.actionGreen 
+          }}
+        /> 
+      </Animated.View>
+      <TouchableOpacity onPress={moveBall}>
+        <Text>click me!</Text>
+      </TouchableOpacity>
+    </View>
+  )
   
+  content = animated
   /************************************************
   ANIMATIONS
   *************************************************/
