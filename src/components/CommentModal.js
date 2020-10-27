62a5a26a0a2ab6e418a71750b753dc0ece3d4308
John Rykken <john.rykken@gmail.com>
2020-10-27 10:04:57 -0700

    successfully show modal. fix 'unrecognized selector' error

--- src/components/CommentModal.js
+++ src/components/CommentModal.js
@@ -1,29 +1,65 @@
 import React, { useState } from 'react'
-import { Modal, View, Dimensions } from 'react-native'
+import { Modal, View, Pressable } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
+import COLORS from '../colors'
+
 
 const CommentModal = ({ width, height }) => {
+  let content
   const marginTop = height * .8
   const marginLeft = width * .8
   const styles = {
-    icon: { marginTop, marginLeft }
+    icon: { 
+      zIndex: 100, 
+      position: 'absolute', 
+      marginTop, 
+      marginLeft, 
+      height: 60, 
+      width: 60 
+    }
   }
 
-  
+  const [modalVisible, setModalVisible] = useState(false)
 
-  console.log('rendering modal')
-  return (
-    <View style={[{position: 'absolute', zIndex: 100 }, styles.icon]}>
+  function toggleModal() {
+    console.log('comment icon pressed!')
+    setModalVisible(!modalVisible)
+  }
+
+  console.log('rendering modal component')
+
+  const commentIcon = (
+    <Pressable 
+      style={styles.icon}  
+      onPress={toggleModal}> 
       <FontAwesome name="commenting-o" size={36} color="white" /> 
-    </View>
-    // <Modal
-    //   transparent={true}
-    //   visible={true} // prop for this
-    //   onRequestClose={() => console.log('modal closed')}
-    //   style={{ zIndex: 100, marginBottom: 20, marginRight: 20, position: 'absolution'}}
-    // >
-    // </Modal>
+    </Pressable>
   )
+
+  const modal = (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible} // double usage of "modalVisible" probably not good
+      presentationStyle="overFullScreen"
+      onRequestClose={() => console.log('modal closed')}
+      style={{ zIndex: 100, marginBottom: 20, marginRight: 20, position: 'absolute'}}>
+        <View style={{flex: 1}}>
+          <View style={{flex: 1 }} />
+          <View style={{flex: 1, flexDirection: 'column', borderRadius: 25, backgroundColor: COLORS.lynxWhite }}/> 
+        </View>
+      
+    </Modal>
+  )
+
+  if (!modalVisible) content = commentIcon
+
+  if (modalVisible) content = modal 
+
+  return content
+  
+  
+  
 }
 
 export default CommentModal
